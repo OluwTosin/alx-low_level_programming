649c2f841eed332a8704dd92e20fe2466c16a0d5
OluwTosin <tosinayobamioluwasola@gmail.com>
2023-08-10 13:16:13 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - taking this as the main
+ *
+ * Return: will be 0 when succesful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
