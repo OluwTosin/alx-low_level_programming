f7338d51ff5d66663473bb34d09c141186ac1c10
OluwTosin <tosinayobamioluwasola@gmail.com>
2023-08-12 15:48:02 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,13 +12,13 @@ int i;
 
 for (i = 0 ; i < 10 ; i++)
 {
-if (i == 9)
 putchar(i + '0');
-putchar(' ');
-else
-putchar(i + '0');
+if (i != 9)
+{
 putchar(',');
 putchar(' ');
 }
+}
+putchar('\n');
 return (0);
 }
