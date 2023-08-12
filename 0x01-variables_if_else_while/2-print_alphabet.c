36df0babba37d2e68b530fc1e96e04d564c3cc0d
OluwTosin <tosinayobamioluwasola@gmail.com>
2023-08-12 01:38:17 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - taking this as the main
+ *
+ * Return: will be 0 when it is sucessfull
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
