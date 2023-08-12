7980d417f6995b8774eaf25fd491a293efab1093
OluwTosin <tosinayobamioluwasola@gmail.com>
2023-08-12 13:03:18 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - taking this as the main
+ *
+ * Return: will be 0 when it is sucessfull
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
