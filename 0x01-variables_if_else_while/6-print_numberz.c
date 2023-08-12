e07579e64ea7077aa98af36f4163e76a00a714c4
OluwTosin <tosinayobamioluwasola@gmail.com>
2023-08-12 13:43:46 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - taking this as the main
+ *
+ * Return: will be 0 when it is sucessfull
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	putchar('\n');
+	return (0);
+}
