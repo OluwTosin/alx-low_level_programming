64e43f24738b87582d80694b7578c7e258b790e3
OluwTosin <tosinayobamioluwasola@gmail.com>
2023-08-16 16:59:08 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,7 +15,14 @@ for (i = 0 ; i < 10 ; i++)
 for (j = 0 ; j < 10 ; j++)
 {
 k = i * j;
+if (j == 0)
 printf("%d, ",k);
+else
+{
+printf("%2d",k);
+if (j != 9)
+printf(", ");
+}
 }
 printf("\n");
 }
