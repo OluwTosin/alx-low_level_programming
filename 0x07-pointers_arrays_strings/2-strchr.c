668a527289cd11b06768fc9a4a39e6dfc8bd1475
OluwTosin <tosinayobamioluwasola@gmail.com>
2023-08-28 05:46:20 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strchr - a function that locates a character in a string
+ * @c: character in the string
+ * @s: the string
+ * Return: s
+ */
+
+char *_strchr(char *s, char c)
+{
+while (*s != '\0')
+{
+if (*s == c)
+return (s);
+}
+return (NULL);
+}
