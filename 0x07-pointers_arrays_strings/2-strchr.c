d1b5a8b55d73213f00b3e4e51224ede83b4d2120
OluwTosin <tosinayobamioluwasola@gmail.com>
2023-08-28 06:02:41 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,7 +4,7 @@
  * _strchr - a function that locates a character in a string
  * @c: character in the string
  * @s: the string
- * Return: s or NULL
+ * Return: s or NULiL
  */
 
 char *_strchr(char *s, char c)
@@ -13,8 +13,7 @@ while (*s != '\0')
 {
 if (*s == c)
 return (s);
+s++;
 }
-if (*s == c)
-return (s);
 return (NULL);
 }
