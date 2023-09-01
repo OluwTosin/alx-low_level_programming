92c3078fc5d554bc36a0f06ab9fd2f4a19e8d10d
OluwTosin <tosinayobamioluwasola@gmail.com>
2023-09-01 14:25:46 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char *argv[])
 {
-int num1, num2, mul = 0;
+int mul = 0, num1, num2;
 
 if (argc != 3)
 {
@@ -22,7 +22,7 @@ else
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
 
-	mul = num1 * num2;
+mul = num1 *num2;
 printf("%d\n", mul);
 }
 return (0);
