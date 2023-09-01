94ae8d13730cd0f3ffb8b5246867d5e96a92d178
OluwTosin <tosinayobamioluwasola@gmail.com>
2023-09-01 11:02:12 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - that prints the number of arguments passed into it
+ * @argc: argument count
+ * @argv: argument v
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argv;
+printf("%d\n", argc);
+return (0);
+}
